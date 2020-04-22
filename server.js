b7b213d5dcc6c469677411e788ddc9bf00a7331d
jvanhook13 <jaredvanhook1313@gmail.com>
2020-04-22 10:50:58 -0700

    sql queries properly pull data out

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 
 var path = require("path");
+var mysql = require("mysql");
 
 var app = express();
 
@@ -9,15 +10,64 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
 
 
 
 
-app.listen(PORT, function() {
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "FriendFinder_db"
+});
+
+
+
+
+app.get("/", function () {
+
+
+    connection.query("SELECT * FROM survey", function (err, data) {
+
+        if (err) throw err; (data) 
+
+        data.forEach(question => { var realquestion = question.question ; console.log(realquestion) });
+
+
+
+    })
+})
+
+app.get("/friends", function () {
+
+
+            
+            connection.query("SELECT * FROM friends", function (err, data) {
+
+                if (err) throw err; (data) 
+        
+                data.forEach(friend => { var realfriend = friend.friendName ; var score = friend.score ; console.log(realfriend , score ) });
+        
+        
+        
+            })
+
+
+
+        })
+    
+
+
+
+
+app.listen(PORT, function () {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
-  });
-  
+});
